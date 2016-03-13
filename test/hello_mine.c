b0daf4e38c1a9eca44db22f5d6ddbc91f0042b87
Spring zhao <zwj.air@gmail.com>
2016-03-13 13:31:52 +0800

    add a test file

---
+++ test/hello_mine.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello_mine.c
+	> Author: zxh
+	> Mail: qmg11@163.com 
+	> Created Time: Thu 07 Jan 2016 04:59:43 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, void **argv)
+{
+	printf("i am child\n");
+	return 1 / 0;
+}
